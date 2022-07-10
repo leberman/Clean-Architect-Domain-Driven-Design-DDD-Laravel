b4e6bab058db38967f2cca8ee509161289453a9d
Mohammad <leberman11@gmail.com>
2022-07-10 21:30:19 +0430

    add User Modeling

---
+++ database/seeders/DefaultUserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DefaultUserSeeder extends Seeder
+{
+
+    public function run() :void
+    {
+        User::factory()->create([
+            'first_name' => 'Mohammad',
+            'last_name' => 'shahbazi',
+            'email' => 'leberman11@gmail.com'
+        ]);
+    }
+}
