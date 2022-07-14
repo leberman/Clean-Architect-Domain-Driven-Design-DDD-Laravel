7df6e2e7a018f900f8383f290bc9d143b442c1fc
Mohammad <leberman11@gmail.com>
2022-07-15 00:21:56 +0430

    user response in api

---
+++ app/Http/Resources/Api/V1/UserResource.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+
+    public function toArray($request) :array
+    {
+        return [
+            'name' => $this->first_name . ' ' . $this->last_name,
+            'email' =>$this->email
+        ];
+    }
+}
